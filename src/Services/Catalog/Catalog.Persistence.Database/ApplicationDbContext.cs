e628d8169b195633f9d1b518ab5f472be6a348bd
mario-galindo <mgalindo9410@gmail.com>
2020-08-30 00:04:45 -0600

    override configuration builder

--- src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
+++ src/Services/Catalog/Catalog.Persistence.Database/ApplicationDbContext.cs
@@ -1,4 +1,5 @@
 ﻿using Catalog.Domain;
+using Catalog.Persistence.Database.Configuration;
 using Microsoft.EntityFrameworkCore;
 
 namespace Catalog.Persistence.Database
@@ -13,5 +14,16 @@ namespace Catalog.Persistence.Database
         public DbSet<Product> Products { get; set; }
         public DbSet<ProductInStock> Stocks { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            ModelConfig(builder);
+        }
+
+        private void ModelConfig(ModelBuilder modelBuilder)
+        {
+            new ProductConfiguration(modelBuilder.Entity<Product>());
+            new ProductInStockConfiguration(modelBuilder.Entity<ProductInStock>());
+        }
     }
 }
