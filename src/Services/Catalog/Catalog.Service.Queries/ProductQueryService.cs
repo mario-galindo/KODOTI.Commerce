eb6bedfc29793705a22889c315e1a72e6add6e94
mario-galindo <mgalindo9410@gmail.com>
2020-08-30 19:12:39 -0600

    add DTOs and Pagination Projects

---
+++ src/Services/Catalog/Catalog.Service.Queries/ProductQueryService.cs
@@ -0,0 +1,14 @@
+﻿using Catalog.Persistence.Database;
+
+namespace Catalog.Service.Queries
+{
+    public class ProductQueryService
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ProductQueryService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
